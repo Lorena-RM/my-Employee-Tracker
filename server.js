755d1a248bd638571e01ffed2ce97e6ac3006cd2
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-11 16:28:15 -0600

    init function and logo updated

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
+//const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+//const db = require("./db");
+//require("console.table");
 
+init();
+
+function init() {
+    const logoText = logo({
+        name: 'Employee Tracker',
+        font: 'ANSI Shadow',
+        lineChars: 10,
+        padding: 2,
+        margin: 3,
+        borderColor: 'grey',
+        logoColor: 'bold-cyan',
+        textColor: 'blue',
+    })
+    .emptyLine()
+    .right('Developed by: Lorena Morales')
+    .right('version 1.0.0')
+    .render()
+ 
+    console.log(logoText);
+  
+    //loadMainUserOptions();
+}
 
