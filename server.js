6e3ee0c5af7b30373a41fff8ed9980d8cc9f76d5
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-11 16:42:56 -0600

    loadmainuserprompts function added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//const { prompt } = require("inquirer");
+const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
 //const db = require("./db");
 //require("console.table");
@@ -6,23 +6,94 @@ const logo = require("asciiart-logo");
 init();
 
 function init() {
-    const logoText = logo({
-        name: 'Employee Tracker',
-        font: 'ANSI Shadow',
-        lineChars: 10,
-        padding: 2,
-        margin: 3,
-        borderColor: 'grey',
-        logoColor: 'bold-cyan',
-        textColor: 'blue',
-    })
+  const logoText = logo({
+    name: "Employee Tracker",
+    font: "ANSI Shadow",
+    lineChars: 10,
+    padding: 2,
+    margin: 3,
+    borderColor: "grey",
+    logoColor: "bold-cyan",
+    textColor: "blue",
+  })
     .emptyLine()
-    .right('Developed by: Lorena Morales')
-    .right('version 1.0.0')
-    .render()
- 
-    console.log(logoText);
-  
-    //loadMainUserOptions();
+    .right("Developed by: Lorena Morales")
+    .right("version 1.0.0")
+    .render();
+
+  console.log(logoText);
+
+  loadMainUserOptions();
 }
 
+function loadMainUserOptions() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES",
+        },
+        {
+          name: "Add Employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Update Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE",
+        },
+        {
+          name: "Remove Employee",
+          value: "REMOVE_EMPLOYEE",
+        },
+        {
+          name: "View All Roles",
+          value: "VIEW_ROLES",
+        },
+        {
+          name: "Add Role",
+          value: "ADD_ROLE",
+        },
+        {
+          name: "Remove Role",
+          value: "REMOVE_ROLE",
+        },
+        {
+          name: "View All Departments",
+          value: "VIEW_DEPARTMENTS",
+        },
+        {
+          name: "Add Department",
+          value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "Remove Department",
+          value: "REMOVE_DEPARTMENT",
+        },
+        {
+          name: "View All Employees By Department",
+          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+        },
+        {
+          name: "View All Employees By Manager",
+          value: "VIEW_EMPLOYEES_BY_MANAGER",
+        },
+        {
+          name: "Update Employee Manager",
+          value: "UPDATE_EMPLOYEE_MANAGER",
+        },
+        {
+          name: "View Total Utilized Budget By Department",
+          value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT",
+        },
+        {
+          name: "Quit",
+          value: "QUIT",
+        },
+      ],
+    },
+  ])
+}
